f4a46c6a05cc652d86a9c2b6603e1721219cdd2a
MuttaqeenChaudhry <chmuttaqeen@gmail.com>
2023-11-20 13:39:31 +0500

    AbtMn Cmplete

--- src/components/AbtMn.js
+++ src/components/AbtMn.js
@@ -1,13 +1,17 @@
 import React, { useEffect } from "react";
 import SpaceStrz from "./SpaceStrz";
+import 'animate.css';
 import "../stylesheets/AbtMn.css";
 import Lottie from "lottie-web";
-import Dev from '../animations/Dev2.json'
-import QualData from '../animations/Stdnt.json'
+import Dev from '../animations/Dev2.json';
+import QualData from '../animations/Stdnt.json';
+import ExpWkinC from '../animations/ExpWkin.json';
+
 
 export default function AbtMn() {
   let DevMe = React.createRef();
   let QualificationContainer = React.createRef();
+  let ExpWkin = React.createRef();
   useEffect(()=>{
     Lottie.loadAnimation({
       container : DevMe.current,
@@ -16,7 +20,66 @@ export default function AbtMn() {
     Lottie.loadAnimation({
       container : QualificationContainer.current,
       animationData : QualData
-    })
+    });
+    Lottie.loadAnimation({
+      container : ExpWkin.current,
+      animationData : ExpWkinC
+    });
+  },[])
+  useEffect(()=>{
+    const TlSegment1 = document.querySelector('#TlSegment1');
+    const TlSegment2 = document.querySelector('#TlSegment2');
+    const TlSegment3 = document.querySelector('#TlSegment3');
+    const options = {
+      root : null,
+      rootMargin : '0px',
+      threshold : 0.7
+    }
+    const observer1 = new IntersectionObserver(callback, options);
+    const observer2 = new IntersectionObserver(callback2, options);
+    const observer3 = new IntersectionObserver(callback3, options);
+    observer1.observe(TlSegment1);
+    observer2.observe(TlSegment2);
+    observer3.observe(TlSegment3);
+    function callback(entries) {
+      console.log(entries)
+      entries.forEach(entry => {
+        if(entry.isIntersecting)
+        {
+          TlSegment1.classList.add('BderFadeIn')
+        }
+        else
+        {
+          // Do Nothing
+        }
+        
+      });
+    };
+    function callback2(entries) {
+      entries.forEach(entry => {
+        if(entry.isIntersecting)
+        {
+          TlSegment2.classList.add('BderTwoFadeIn')
+        }
+        else
+        {
+          // Do Nothing
+        }
+        
+      });
+    };function callback3(entries) {
+      entries.forEach(entry => {
+        if(entry.isIntersecting)
+        {
+          TlSegment3.classList.add('BderThreeFadeIn')
+        }
+        else
+        {
+          // Do Nothing
+        }
+        
+      });
+    };
   },[])
   return (
     <>
@@ -25,9 +88,9 @@ export default function AbtMn() {
         <div className="Abt_Main">
           <div className="Abt_Mn_Sec1">
             <div className="TimelineContainer">
-              <div className="TimeLineSegment Timeline ">
-                <div style={{ width: "50%" }}>
-                  <h4 className="c-prim f-sec">Introduction</h4>
+              <div id='TlSegment1' className="TimeLineSegment">
+                <div className="Timeline">
+                  <h4 style={{color: '#1F305E'}} className="f-sec">Introduction</h4>
                   <p className="f-ub">
                     Hello there, I'm Muhammad Muttaqeen, a 23-year-old Muslim
                     with a fervent passion for Web Application Development. My
@@ -42,9 +105,9 @@ export default function AbtMn() {
                   <div ref={DevMe} />
                 </div>
               </div>
-              <div className="TimeLineSegment Timeline">
-                <div style={{ width: "50%" }}>
-                  <h4 className="c-prim f-sec">Qualifications</h4>
+              <div id='TlSegment2' className="TimeLineSegment">
+                <div className='Timeline'>
+                  <h4 style={{color: '#0076CE'}} className="f-sec">Qualifications</h4>
                   <p className="f-ub">
                     I completed my Matriculation with a focus on Computer
                     Science, laying the foundation for my academic journey.
@@ -63,8 +126,8 @@ export default function AbtMn() {
                   <div ref={QualificationContainer} />
                 </div>
               </div>
-              <div className="TimeLineSegment Timeline">
-                <div style={{ width: "50%" }}>
+              <div id='TlSegment3' className="TimeLineSegment">
+                <div className='Timeline'>
                   <h4 className="c-prim f-sec">Experience</h4>
                   <p className="f-ub">
                     I began my web development journey with my Final Year
@@ -81,7 +144,9 @@ export default function AbtMn() {
                     solutions and continuous skill enhancement .
                   </p>
                 </div>
-                <div style={{ width: "50%" }}>as</div>
+                <div style={{ width: "50%" }}>
+                  <div ref={ExpWkin}/>
+                </div>
               </div>
             </div>
           </div>
