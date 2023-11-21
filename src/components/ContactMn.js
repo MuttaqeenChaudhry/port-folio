4345cdebd7accd24ae905a8e20c4d8c3cf425286
MuttaqeenChaudhry <chmuttaqeen@gmail.com>
2023-11-21 13:45:19 +0500

    FormChanges

--- src/components/ContactMn.js
+++ src/components/ContactMn.js
@@ -19,16 +19,20 @@ export default function ContactMn() {
       <SpaceStrz numberofstars={5} Prim="Reach" Sec='Out'/>
       <div className="ContactMain">
         <div className='ContactCntnt'>
-          <div style={{width:'50%', padding:'5px'}}>
+          <div className='CntctSec1'>
             <form action="">
               <fieldset>
               <legend>Fill out the form</legend>
               <label className='c-prim f-sec' htmlFor="Data1">Subject</label><br/>
+              <span  style={{position:'relative'}}>
               <input placeholder='*' type="text" id='Data1' required /><br/>
+              </span>
               <label className='c-prim f-sec'>Name</label><br/>
               <input type="text" name="" id="" /><br/>
               <label className='c-prim f-sec'>Email</label><br/>
+              <span style={{position:'relative'}}>
               <input placeholder='*' type="email" name="" id="" required/><br/>
+              </span>
               <label className='c-prim f-sec' htmlFor="">Your Message:</label><br/>
               <textarea rows='5' cols='100%' style={{resize:'none'}}></textarea><br/>
               <input className='c-prim f-sec hveffect' type="submit" value="Submit" />
@@ -36,7 +40,7 @@ export default function ContactMn() {
             </form>
 
           </div>
-          <div style={{width:'50%', padding:'5px'}}>
+          <div className='CntctSec2'>
           <div ref={FmAnim} />
           </div>
         </div>
